b06d291c5e9b49d28554c399dae55d4588f93ae6
Klee Collage <klee.collage@gmail.com>
2024-12-07 12:07:44 -0600

    Crons

--- src/modules/cron/cron.controller.ts
+++ src/modules/cron/cron.controller.ts
@@ -1,7 +1,88 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Put } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { CronService } from 'src/modules/cron/cron.service';
 
 @Controller('api/v1/cron')
 export class CronController {
   constructor(private cronService: CronService) {}
+  //** ------------------------------ GET ALL CRON NAMES ------------------------------ **//
+  @Get()
+  @ApiOperation({
+    description: 'Obains the name of all crons',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Names returned successfully',
+  })
+  getNamesCrons() {
+    return this.cronService.getNamesCrons();
+  }
+  //** ------------------------------ DESACTIVATE CRONE ------------------------------ **//
+  @Put('/desactivate/:name')
+  @ApiOperation({
+    description: 'Deactivates one cron',
+  })
+  @ApiParam({
+    name: 'name',
+    type: String,
+    required: true,
+    description: 'Cron name to deactivate',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Cron deactivated correctly',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Cron not exists',
+  })
+  desactivateCron(@Param('name') name: string) {
+    return this.cronService.desactivateCron(name);
+  }
+  //** ------------------------------ ACTIVATE CRONE ------------------------------ **//
+  @Put('/activate/:name')
+  @ApiOperation({
+    description: 'Activate one cron',
+  })
+  @ApiParam({
+    name: 'name',
+    type: String,
+    required: true,
+    description: 'Cron name to activate',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Cron activated correctly',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Cron not exists',
+  })
+  activateCron(@Param('name') name: string) {
+    return this.cronService.activateCron(name);
+  }
+  //** ------------------------------ DESACTIVATE ALL CRONE ------------------------------ **//
+  @Put('/desactivate-all')
+  @ApiOperation({
+    description: 'Desactivate all active crons',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Crons disabled correctly',
+  })
+  desactivateAllCrons() {
+    return this.cronService.desactivateAllCrons();
+  }
+  //** ------------------------------ ACTIVATE ALL CRONE ------------------------------ **//
+  @Put('/activate-all')
+  @ApiOperation({
+    description: 'Activate all disabled crons',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Crons disabled correctly',
+  })
+  activateAllCrons() {
+    return this.cronService.activateAllCrons();
+  }
 }
--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -1,6 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class CronService {
-  constructor() {}
+  constructor(private schedulerRegistry: SchedulerRegistry) {}
+
+  @Cron('*/10 * * * * *', { name: 'cron1' }) // cada 10 secs
+  cron1() {
+    console.log('Cron1: Accion cada 10 s');
+  }
+
+  @Cron('*/30 * * * * *', { name: 'cron2' }) // cada 30 secs
+  cron2() {
+    console.log('Cron2: Accion cada 30 s');
+  }
+
+  @Cron('* * * * *', { name: 'cron3' }) // cada 1 min
+  cron3() {
+    console.log('Cron3: Accion cada 1 min');
+  }
+
+  desactivateCron(name: string) {
+    const job = this.schedulerRegistry.getCronJob(name);
+    if (!job) throw new ConflictException('Cron not exists');
+    else {
+      job.stop();
+      console.log(`Cron ${name} desactivated`);
+      return true;
+    }
+  }
+
+  activateCron(name: string) {
+    const job = this.schedulerRegistry.getCronJob(name);
+    if (!job) throw new ConflictException('Cron not exists');
+    else {
+      job.start();
+      console.log(`Cron ${name} activated`);
+      return true;
+    }
+  }
+
+  getNamesCrons() {
+    const names = [];
+    for (const nameCron of this.schedulerRegistry.getCronJobs().keys()) {
+      names.push(nameCron);
+    }
+
+    return names;
+  }
+
+  desactivateAllCrons() {
+    const names = this.getNamesCrons();
+
+    for (const name of names) {
+      this.desactivateCron(name);
+    }
+
+    return true;
+  }
+
+  activateAllCrons() {
+    const names = this.getNamesCrons();
+
+    for (const name of names) {
+      this.activateCron(name);
+    }
+
+    return true;
+  }
 }
