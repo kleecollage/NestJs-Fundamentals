74184fd7cfbc6e0fb2acae600f81dec7d57161d6
Klee Collage <klee.collage@gmail.com>
2024-12-06 18:03:09 -0600

    First commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Upload files')
+    .setDescription('Example upload files')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
